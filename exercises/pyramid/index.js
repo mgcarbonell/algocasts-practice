177043ae1037273073284c9739267fff0d6e9199
mgcarbonell <mgcarbonell@gmail.com>
2021-08-30 19:46:18 -0700

    completes pyramid iterative and recursive

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -22,7 +22,64 @@
 //        ELSE
 //            Add a space (' ') to our empty string
 //    console.log empty string
+// n * 2 - 1 = our base level
+/**
+ * n = 2
+ * n * 2 = 4 - 1 = 3
+ *  #
+ * ###
+ * 
+ * So the other way to say that (n * 2) - 1 = base our level
+ * (n * 2) - 1 = to our total col
+ * n = rows
+ * for placement of the # we can do something like: Math.floor((2 * n - 1) / 2)
+ */
 
-function pyramid(n) { }
+function pyramid(n) {
+  // n = row
+  // (n * 2) - 1 = col
+  const mid = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level)
+  }
+}
+
+// recursive function
+// function pyramid(n, row = 0, level = '') {
+//   // base case
+//   if (row === n) {
+//     return;
+//   }
+
+//   // recursive
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+
+//   // state variables
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   let add;
+
+//   // add ' ' or '#' to level
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = '#';
+//   } else {
+//     add = ' ';
+//   }
+
+//   return pyramid(n, row, level + add);
+// }
 
 module.exports = pyramid;
+
