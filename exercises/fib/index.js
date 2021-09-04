b1bebc55e6c464ac95729fe7bc53971ce0ab3819
mgcarbonell <mgcarbonell@gmail.com>
2021-09-03 21:44:33 -0700

    Adds memoize function for fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,7 @@
 // Example:
 //   fib(4) === 3
 
+// time complexity: O(2**n)
 // function fib(n) {
 //   if (n < 2) {
 //     return n;
@@ -15,8 +16,43 @@
 //   return (fib(n - 1) + fib(n - 2))
 // }
 
-// iterative refactor
-// there's a trick: the first two numbers cannot be generated (0, 1)
+// memoized
+// how do improve the recursive solution from being exponential?
+// store arguments of each function along with the result
+// if the function is called again with the same arguments, return the
+// precomputed result instead of running the function again
+
+// let's write a generic memoize function that we can use AGAIN OVER AND OVER
+// our memoize function will take in a function (fn) and RETURN A FUNCTION
+// our memoize function will use a CACHE (an object O(1) look up)
+// we will also use a spread args on our function because we don't "know"
+// how many arguments our function will take, but it will take some
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) { // check cache
+      return cache[args]; // if it exists, return it
+    }
+    // if it passes, we've never called it before and need to cache
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function fib(n) {
+  if (n < 2) return n;
+
+  return (fib(n - 1) + fib(n - 2));
+}
+
+fib = memoize(fib);
+
+
+// iterative solution presented in class
+// there's a trick: the first two numbers cannot be generated (0, 1) so we add
+// them to our result array straight off
 // run time = O(n)
 // function fib(n) {
 //   const result = [0, 1];
