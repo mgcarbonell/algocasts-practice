80527ee3263fbe67556a4189a2bab6584dc58d49
mgcarbonell <mgcarbonell@gmail.com>
2021-09-09 14:15:06 -0700

    completes tree exercises

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,31 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+
+// we can't really just simply go "Oh okay so it's like an insert method."
+// If we go left, max = parent 
+// If we go right, min = child
+// Anytime we move left we update max
+// Anytime we move right we update min
+
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
