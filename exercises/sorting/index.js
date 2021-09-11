1e89675874f083eabc776689ccbb34e49301ce11
mgcarbonell <mgcarbonell@gmail.com>
2021-09-11 15:05:32 -0700

    Completes selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,7 +3,8 @@
 /*
 loop through the array
   set up an inner loop to go from 0-> arr.length - i
-
+    if current elem is larger than elem + 1
+      swap
 */
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -15,12 +16,33 @@ function bubbleSort(arr) {
       }
     }
   }
-
   return arr;
 }
 
+/*
+From i = 0 < arr.length;
+  Assume elem @ i is the smallest of the array (indexOfMin)
+  For j from i + 1 -> end of arr.length
+    See if there is an elem with lower value
+      If there is, assign indexOfMin
+  If idx of cur and idx of indexOfMine
+    swap
+*/
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      const temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
