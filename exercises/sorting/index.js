c59eef5e8a0b33efd69961e1cf1c9e04f1470f93
mgcarbonell <mgcarbonell@gmail.com>
2021-09-11 16:23:24 -0700

    completes merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -45,12 +45,46 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {
+/*
+Merge sort is usually done via recursive solution that uses two functions: merge and mergeSort. Each of these functions have a different purpose.
 
+Within mergeSort is where the recursion will happen. Within the merge function there will be no recursion. 
+
+in our merge function:
+This will take in two sorted arrays (left and right)
+Create a 'results' array
+While there are still elements in /both/ arrays
+  If the first elem of the left half < first in right half
+    'shift' elem from left to results
+  else
+    'shift' elem from right into results arr
+Take everything from the array that still has stuff in it and put it into results
+this will look like: return [...results, ...left, ...right];
+
+mergeSort function:
+split the array into the smallest possible elem (arr.length = 1);
+*/
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const results = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
